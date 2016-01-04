1965b377b446a0bfab00477a7336878bdc628ccd
panicoenlaxbox <panicoenlaxbox@gmail.com>
2016-01-04 18:27:40 +0100

    Area

---
+++ CodeOrganization/Areas/Admin/Features/Users/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CodeOrganization.Areas.Admin.Features.Users
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
