b368e6ccb7d1a1e503417e427ef79204e9605b96
panicoenlaxbox <panicoenlaxbox@gmail.com>
2016-01-04 18:54:37 +0100

    Controlador en Área con el mismo nombre que un controlador fuera

--- CodeOrganization/App_Start/RouteConfig.cs
+++ CodeOrganization/App_Start/RouteConfig.cs
@@ -16,7 +16,12 @@ namespace CodeOrganization
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, },
+                namespaces: new[]
+                {
+                    "CodeOrganization.Controllers", 
+                    "CodeOrganization.Features.*"
+                }
             );
         }
     }
